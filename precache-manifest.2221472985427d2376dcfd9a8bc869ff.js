e698b0d37bf57350f18794139a45b7bfd3033ce8
giahungbk <giahung197bg@gmail.com>
2020-04-17 12:38:42 +0700

    Updates

---
+++ precache-manifest.2221472985427d2376dcfd9a8bc869ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb3eef317be63687046779aedd8c26e",
+    "url": "/tab-gallery/index.html"
+  },
+  {
+    "revision": "17856eba56810c9c76e2",
+    "url": "/tab-gallery/static/css/main.60c24307.chunk.css"
+  },
+  {
+    "revision": "a056c047dbcd23671368",
+    "url": "/tab-gallery/static/js/2.f4e86e5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tab-gallery/static/js/2.f4e86e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17856eba56810c9c76e2",
+    "url": "/tab-gallery/static/js/main.d5a153d2.chunk.js"
+  },
+  {
+    "revision": "44c1233ec96f43525951",
+    "url": "/tab-gallery/static/js/runtime-main.d8605a68.js"
+  },
+  {
+    "revision": "e5aca032544e5ea7c347559846146e6f",
+    "url": "/tab-gallery/static/media/img1.e5aca032.jpg"
+  },
+  {
+    "revision": "eea3e601b2c5eb7cb5ce15de6f9034d2",
+    "url": "/tab-gallery/static/media/img2.eea3e601.jpg"
+  },
+  {
+    "revision": "839029694fc5c2ae56140dba79987403",
+    "url": "/tab-gallery/static/media/img3.83902969.jpg"
+  }
+]);
